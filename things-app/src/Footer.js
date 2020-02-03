2e88def214c9360388ec9ac007a59abc25adc2c2
Ting Luo <tin831@gmail.com>
2020-02-03 07:36:59 -0800

    added more features

--- things-app/src/Footer.js
+++ things-app/src/Footer.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
-class Footer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      bye: 'Thanks for visiting!',
-    };
-  }
+// export default props => (
+//   <>
+//     <h1>Bye!</h1>
+//   </>
+// )
 
-  render() {
-    return (
-      <h1>{this.state.bye}</h1>
-    )
-  }
+function Footer(props) {
+  return (
+    <>
+      <h1>{props.bye}</h1>
+    </>
+  )
 }
 
 export default Footer
